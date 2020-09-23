464408e39c458b4fe62fce101485f5352cd540af
Sandy Jacobson <sandyjacobson@Sandys-MBP.home>
2020-09-23 15:21:00 -0400

    redirect

--- client/src/screens/PostCreate/PostCreate.jsx
+++ client/src/screens/PostCreate/PostCreate.jsx
@@ -28,7 +28,7 @@ const PostCreate = (props) => {
     }
 
     if (isCreated) {
-        return <Redirect to={`/posts`} />
+        return <Redirect to={`/`} />
     }
     return (
         <Layout>
