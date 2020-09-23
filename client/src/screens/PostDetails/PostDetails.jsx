20cd3c97de7401d1b85f66cb11dbab182dd2dc5b
Sandy Jacobson <sandyjacobson@Sandys-MBP.home>
2020-09-23 13:51:43 -0400

    new pics

--- client/src/screens/PostDetails/PostDetails.jsx
+++ client/src/screens/PostDetails/PostDetails.jsx
@@ -1,51 +1,51 @@
-// import React, { useState, useEffect } from "react";
-// import { useParams } from "react-router-dom";
-// import { getPost } from "../../services/posts";
-// import Layout from "../../components/shared/Layout/Layout";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getPost } from "../../services/posts";
+import Layout from "../../components/shared/Layout/Layout";
 
-// const PostDetails = (props) => {
-//   const [post, setPost] = useState(null);
-//   const [isLoaded, setLoaded] = useState(false);
-//   const { id } = useParams();
+const PostDetails = (props) => {
+  const [post, setPost] = useState(null);
+  const [isLoaded, setLoaded] = useState(false);
+  const { id } = useParams();
 
-//   useEffect(() => {
-//     const fetchPost = async () => {
-//       const post = await getPost(id);
-//       console.log(getPost)
-//       setPost(post);
-//       setLoaded(true);
-//     }
-//     fetchPost()
-//   }, [id]);
+  useEffect(() => {
+    const fetchPost = async () => {
+      const post = await getPost(id);
+      console.log(getPost)
+      setPost(post);
+      setLoaded(true);
+    }
+    fetchPost()
+  }, [id]);
 
-//   if (!isLoaded) {
-//     return <h1>... not loading</h1>;
-//   }
+  if (!isLoaded) {
+    return <h1>... not loading</h1>;
+  }
 
-//   const posts = () => {
-//     return (
-//       <div className="single-post">
-//       <ul>
-//         <li>{post.title}</li>
-//           <img src={post.imgURL} alt='robocophousinggames' />
-//           <li>{post.content}</li>
-//       </ul>
-//       </div>
-//     )
-//   };
+  const posts = () => {
+    return (
+      <div className="single-post">
+      <ul>
+        <li>{post.title}</li>
+          <img src={post.imgURL} alt='robocophousinggames' />
+          <li>{post.content}</li>
+      </ul>
+      </div>
+    )
+  };
 
-//   return (
-//     <Layout>
-//       <div className="single-post">
-//         <ul>
-//         <li>{post.title}</li>
-//         <img src={post.imgURL} alt="robocophousinggames" />
-//          <li>{post.content}</li>
-//         </ul>
-//       </div>
-//     </Layout>
-//   );
-// };
+  return (
+    <Layout>
+      <div className="single-post">
+        <ul>
+        <li>{post.title}</li>
+        <img src={post.imgURL} alt="robocophousinggames" />
+         <li>{post.content}</li>
+        </ul>
+      </div>
+    </Layout>
+  );
+};
   
 
-// export default PostDetails;
+export default PostDetails;
