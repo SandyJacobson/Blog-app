a8d4deb609668160b288aef5d098f2266262822c
Sandy Jacobson <sandyjacobson@Sandys-MBP.home>
2020-09-23 14:46:14 -0400

    delete/edit functionality

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./screens/Home/Home";
 import PostDetails from "./screens/PostDetails/PostDetails";
+import PostEdit from './screens/PostEdit/PostEdit'
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/posts/:id' component={PostDetails} />
+        <Route exact path="/posts/:id/edit" component={PostEdit} />
       </Switch>
     </div>
   );
--- client/src/screens/PostDetails/PostDetails.jsx
+++ client/src/screens/PostDetails/PostDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { getPost } from "../../services/posts";
+import { useParams, Link } from "react-router-dom";
+import { getPost, deletePost } from "../../services/posts";
 import Layout from "../../components/shared/Layout/Layout";
 
 const PostDetails = (props) => {
@@ -11,41 +11,41 @@ const PostDetails = (props) => {
   useEffect(() => {
     const fetchPost = async () => {
       const post = await getPost(id);
-      console.log(getPost)
+      console.log(getPost);
       setPost(post);
       setLoaded(true);
-    }
-    fetchPost()
+    };
+    fetchPost();
   }, [id]);
 
   if (!isLoaded) {
     return <h1>... not loading</h1>;
   }
 
-  // const posts = () => {
-  //   return (
-  //     <div className="single-post">
-  //     <ul>
-  //       <li>{post.title}</li>
-  //         <img src={post.imgURL} alt='robocophousinggames' />
-  //         <li>{post.content}</li>
-  //     </ul>
-  //     </div>
-  //   )
-  // };
-
   return (
     <Layout>
       <div className="single-post">
         <ul>
-        <li>{post.title}</li>
-        <img src={post.imgURL} alt="robocophousinggames" />
-         <li>{post.content}</li>
+          <li>{post.title}</li>
+          <img src={post.imgURL} alt="robocophousinggames" />
+          <li>{post.content}</li>
         </ul>
+        <div className="button-container">
+          <button className="edit-button">
+            <Link className="edit-link" to={`/posts/${post._id}/edit`}>
+              Edit
+            </Link>
+          </button>
+          <button
+            className="delete-button"
+            onClick={() => deletePost(post._id)}
+          >
+            Delete
+          </button>
+        </div>
       </div>
     </Layout>
   );
 };
-  
 
 export default PostDetails;
